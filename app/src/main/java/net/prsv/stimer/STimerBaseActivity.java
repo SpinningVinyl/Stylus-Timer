35c73d91dfb3591f7da3302fa7d847dc241bb141
Pavel Urusov <pavel.urusov@gmail.com>
2023-04-01 16:11:35 +0100

    JavaDoc comments in STimerBaseActivity.java

--- app/src/main/java/net/prsv/stimer/STimerBaseActivity.java
+++ app/src/main/java/net/prsv/stimer/STimerBaseActivity.java
@@ -3,16 +3,19 @@ package net.prsv.stimer;
 import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Base activity class extended by all activities in the app.
+ */
 public abstract class STimerBaseActivity extends AppCompatActivity {
+
 
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // lock the screen orientation in portrait
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
