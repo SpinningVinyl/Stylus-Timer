ad7c0da40f156047939eba6c5f013b3a88f485ad
Pavel Urusov <pavel.urusov@gmail.com>
2023-04-01 16:11:13 +0100

    JavaDoc comments in StylusProfile.java

--- app/src/main/java/net/prsv/stimer/StylusProfile.java
+++ app/src/main/java/net/prsv/stimer/StylusProfile.java
@@ -2,30 +2,51 @@ package net.prsv.stimer;
 
 import androidx.annotation.NonNull;
 
+/**
+ * {@link StylusProfile} represents a single stylus profile in the database.
+ * Each stylus profile has three parameters: ID, name and threshold (the number of hours a stylus of this profile can be used before replacement)
+ * @author Pavel Urusov 2023
+ */
 public class StylusProfile {
 
     private int id;
     private String name;
     private int threshold;
 
-    public StylusProfile() {
-        // empty default constructor required for calls to DataSnapshot.getValue(StylusProfile.class)
-    }
+    /**
+     * This empty default constructor is required for calls to DataSnapshot.getValue(StylusProfile.class)
+     */
+    public StylusProfile() {}
 
+    /**
+     * Create a new {@link StylusProfile} object.
+     * @param id profile ID
+     * @param name name of the profile
+     * @param threshold threshold (the number of hours a stylus of this profile can be used before replacement)
+     */
     public StylusProfile(int id, String name, int threshold) {
         this.name = name;
         this.threshold = threshold;
         this.id = id;
     }
 
+    /**
+     * Returns the ID of the profile
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns the name of the profile
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the threshold (the number of hours a stylus of this profile can be used before replacement)
+     */
     public int getThreshold() {
         return threshold;
     }
@@ -33,7 +54,7 @@ public class StylusProfile {
     @NonNull
     @Override
     public String toString() {
-        return this.id + ": " + this.name + ", " + threshold + " hours";
+        return this.name;
     }
 
 }
